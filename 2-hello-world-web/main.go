e25d8afdb543dbe1b77327db507f4a07990a243a
Ko Zin Thet <zin@clickrmedia.com>
2023-12-31 22:40:04 +0630

    24. Error checking

--- 2-hello-world-web/main.go
+++ 2-hello-world-web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -16,6 +17,25 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot dividy by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide bby sero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func addValue(x, y int) int {
 	return x + y
 }
@@ -23,6 +43,7 @@ func addValue(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Sprintf("Starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
